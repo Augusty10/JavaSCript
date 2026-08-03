001dec7f197321a22b647f036bddb464078038ad
Dhanraj  Sahu <rajofficials23@gmail.com>
2026-08-03 22:49:19 +0530

    Add initial implementation of memory management concepts in JavaScript

---
+++ Basic/memory.js
@@ -0,0 +1,21 @@
+// Stack (primetive ) Heap (Non - Primitive )
+
+let Book = "Sidharta "
+let novel = Book
+novel ="The Alchemist"
+
+console.log(Book);
+console.log(novel);
+
+
+let Fiction ={
+    title: "The Alchemist",
+    author: "Paulo Coelho"
+}
+
+let Novel = Fiction
+Novel.title="the Monk who Sold my ferrari  "
+
+
+console.log(Fiction); 
+console.log(Novel); 
