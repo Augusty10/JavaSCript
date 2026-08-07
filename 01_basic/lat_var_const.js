b541ec71d696960c3a2fe04e9cc1c910cce34d99
Dhanraj  Sahu <rajofficials23@gmail.com>
2026-08-07 10:24:58 +0530

    Add variable and constant demonstration in lat_var_const.js

---
+++ 01_basic/lat_var_const.js
@@ -0,0 +1,22 @@
+// in java Scriot   store the  value through variable and constant.
+// there is  3 types of variable in java Script 1. var  2. let  3. const 
+// var is used to store the value of variable and iit van be changed in futre.
+// let is uded to strore the value of vstisble and ot  can be changed in future.
+// const is used to store the value of variable and it can nnot be changed in future.
+// we can use  the  variable and const in java Script to store the value of variable and constant . but we cannot use var cuz of scope issue,
+// Scope issue means if we declare the variable in function then it can be used only in that function and it cannot be used outside of that function. but if we declare the variable outside of the function then it can be used anywhere in the code. but if we declare the variable inside of the function then it can be used only in that function and it cannot be used outside of that function. but if we declare the variable outside of the function then it can be used anywhere in the code. but if we declare the variable inside of the function then it can be used only in that function and it cannot be used outside of that function. but if we declare the variable outside of the function then it can be used anywhere in the code. but if we declare the variable inside of the function then it can be used only in that function and it cannot be used outside of that function. but if we declare the variable outside of the function then it can be used anywhere in the code. but if we declare the variable inside of the function then it can be used only in that function and it cannot be used outside of that function. but if we declare the variable outside of the function then it can be used anywhere in the code. but if we declare the variable inside of the function then it can be used only in that function and it cannot be used outside of that function. but if we declare the variable outside of the function then it can be used anywhere in the code. but if we declare the variable inside of the function then it can be used only in that function and it cannot be used outside of that function. but if we declare the variable outside of the function then it can be used anywhere in the code. but if we declare the variable inside of the function then it can be used only in that function and it cannot be used outside of that function. but if we declare the variable outside of the function then it can be used anywhere in
+
+//  bioData 
+
+let userName = "Laxmi Sen ";
+const UserID = " ABC123";
+let   userAge = 27;
+let userEmail = "laxmi.sen@example.com";
+let treeCount=50;
+
+
+console.table([ userName, UserID , userAge, userEmail, treeCount])
+
+
+
+
