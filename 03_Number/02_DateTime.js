92d2c24861950d7dcabeabcd09f9b63fed09f1df
Dhanraj  Sahu <rajofficials23@gmail.com>
2026-08-07 16:20:40 +0530

    Add initial date handling examples in 02_DateTime.js

---
+++ 03_Number/02_DateTime.js
@@ -0,0 +1,11 @@
+// dates 
+
+let myDate =  new Date ();
+console.log(myDate.toDateString());
+console.log(myDate.toTimeString ());
+console.log(typeof myDate);
+
+let myCreatedDate = new Date (2023, 0, 1) ; // year, month, day
+console.log(myCreatedDate.toDateString());
+ 
+
