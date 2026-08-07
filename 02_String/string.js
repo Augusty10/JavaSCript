a8eca963cb526da1f1ae020890c04bb853c4e092
Dhanraj  Sahu <rajofficials23@gmail.com>
2026-08-07 15:42:28 +0530

    Add new string manipulation examples in string.js

---
+++ 02_String/string.js
@@ -0,0 +1,16 @@
+const name = " Raj "
+const repoCount = 50
+
+console.log(name + "has"+ repoCount +" repositories")    // old Method of concatenation 
+
+console.log(` hello my name ${name} and my repo count is  ${repoCount}`);   // String interpolation 
+
+
+const GameName = new String("Call of Duty") ;
+
+console.log(GameName.valueOf()[0]) // 
+console.log(GameName.__proto__) // prototype of the String object 
+console.log(GameName.toUpperCase()) // convert the string to upper case 
+
+
+
