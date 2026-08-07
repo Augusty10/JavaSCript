1c648b9270f384bc1b2b178fef7bc0a246d9bc7a
Dhanraj  Sahu <rajofficials23@gmail.com>
2026-08-07 11:04:21 +0530

    Remove outdated variable and constant demonstration; add new examples for data types and type conversion

---
+++ 01_basic/03_DataTypes.js
@@ -0,0 +1,18 @@
+ 
+
+ let userName = " Dhanraj Sahu ";
+ let userAge = 25;
+ let userEmail = "dhanraj.sahu@example.com";
+ let Desgination = "Software Engineer";
+ let  isActive = true;
+ let sallry = null;
+ let Success =  undefined 
+
+ 
+ console.log(typeof (userName))
+ console.log(typeof (userAge))
+ console.log(typeof (userEmail))
+ console.log(typeof (Desgination))
+ console.log(typeof (isActive))
+
+
---
+++ 01_basic/04_typeConversion.js
@@ -0,0 +1,8 @@
+let Score = "33" ;
+
+console.log(typeof Score);
+console.log(typeof(Score)); 
+
+let isNumber = Number (Score)
+
+console.log(typeof isNumber)
