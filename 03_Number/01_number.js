e6275123a6107e1b38c8fa78105c9e49a958bf5a
Dhanraj  Sahu <rajofficials23@gmail.com>
2026-08-07 16:07:45 +0530

    Add number manipulation examples in 01_number.js

---
+++ 03_Number/01_number.js
@@ -0,0 +1,26 @@
+const   score = 300
+
+const Ballance = new Number(score)
+
+console.log(Ballance.toString().length ) ; // convevrt the number to the String 
+console.log(Ballance.toFixed(2)) ; // convert the number to fixed decimal points 
+
+
+const otherNumber = 23.8966 
+
+
+console.log(otherNumber.toPrecision(3)) ; // convert the number to a specified precision 
+
+const hundresds = 100000 
+console.log(hundreds.toLocaleString('en-IN')) ; // convert the number to a locale string format
+
+
+// Math Object 
+
+console.log(Math.PI) ;
+console.log(Math.abs(-4)) ;
+console.log(Math.ceil(4.7)) ;
+
+
+console.log(Math.floor (Math.random()* max - min +1 )+ min)
+
