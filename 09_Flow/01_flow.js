fa438a6527c9d187908eae47192f6c7582d76e34
Dhanraj  Sahu <rajofficials23@gmail.com>
2026-08-17 02:25:10 +0530

    Add control flow examples in 01_flow.js and 02_switch.js

---
+++ 09_Flow/01_flow.js
@@ -0,0 +1,83 @@
+// // Control flow 
+
+// // if Statement 
+
+// const isUserlooggedin = true 
+
+// const temprature = 41
+
+
+// if( temprature === 41){
+//  console.log(" temprature is less then 50 ");
+// } else{
+//     console.log(" Temprature is Greater then 50 ");
+// }
+// console.log(" Execute ! ");
+
+
+
+// /*
+// 2<=2 
+
+// 3! =2
+
+// <, > , <= , >= , ==  , !=  , === ( chck type also ) , !== 
+
+
+// */
+
+//  const Score = 200 
+
+//  if(score > 100 ){
+//     const power = " Fly "
+//     console.log(` User power : ${power} `);
+
+//  }
+
+//  console.log(" out of Scope "); 
+
+
+const balance = 1000 
+
+// if (balance> 500) console.log(" test");  // implicit Scope 
+
+
+// Nesting  
+ 
+// if(balance <500){
+//     console.log(" less than : ");
+    
+
+// } else if( balance <75){
+//     " Less then 750"
+// }
+// else  (balance <900)
+// {
+//     console.log(" less then 1222");
+// }
+
+
+const userLoggedIn  = true 
+const debitcard = true 
+const looggedInfromGoogvle  = flase 
+const loggedInFromEMail = true 
+
+
+if (userLoggedIn && debitcard  && 2 ==2 ){
+    console.log(" allow to Shooping ");
+}
+if(loggedInFromEMail  || looggedInfromGoogvle   ){
+    console.log(" User logged In ");
+}
+
+
+
+
+ 
+
+
+
+
+
+
+
---
+++ 09_Flow/02_switch.js
@@ -0,0 +1,26 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = " "
+
+switch(month ){
+     case 1:
+        console.log("Januray ");
+        break;
+     case 2:
+        console.log("febraruary  ");
+        break;
+        
+     case 3:
+        console.log("march  ");
+        break;
+    default:
+        console.log("aprail ");
+}
+
