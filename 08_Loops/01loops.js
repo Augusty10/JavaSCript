1061bf8e98e23af92b5e765d58ec3a9a14977deb
Dhanraj  Sahu <rajofficials23@gmail.com>
2026-08-17 17:48:26 +0530

    Refactor for loop examples and add break/continue demonstrations in 01loops.js

--- 08_Loops/01loops.js
+++ 08_Loops/01loops.js
@@ -1,5 +1,62 @@
 // for loop 
 
-for (let index = 0; index < array.length; index++) {
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    if(element ==5){
+        console.log(" 5 is best Number ");
+        
+    }
+    // console.log(element);
     
 }
+
+// console.log(element);
+
+
+
+for(let i = 0 ; i<=10 ; i++){
+
+console.log(` Outer loop value : ${ i}`);
+
+    for(let j = 0 ; j<=10; j++){
+        // console.log(` innner loop value ${j} is inner loop ${i}`);
+        // console.log( i + ` *`+ j ` = ` + i*j );
+    }
+}
+
+
+
+let myarray =[" flash", "batman " , " Superman " ]
+// console.log(myarray.length);
+
+for (let index = 0; index < myarray.length; index++) {
+    const element = myarray[index];
+    // console.log(element);
+}
+
+
+// Break and Continue 
+
+ for (let index = 1;  index <= 20; index ++ ){
+
+    if (index == 5){
+        console.log(` Detected 5 `);
+        break
+
+    }
+     console.log(`value of i is ${index}`);
+     
+ }
+// Break and Continue 
+
+ for (let index = 1;  index <= 20; index ++ ){
+
+    if (index == 5){
+        console.log(` Detected 5 `);
+        continue
+
+    }
+     console.log(`value of i is ${index}`);
+     
+ }
+
